eead2b177a10df7f253e4669f7c54f19f9df7982
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-25 18:41:29 -0700

    Changed over to modules

--- back_end/prisma-client/prismainstance.js
+++ back_end/prisma-client/prismainstance.js
@@ -1,9 +1,10 @@
-const {PrismaClient} = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 require("dotenv").config();
 const databaseUrl =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL;
+
 const prisma = new PrismaClient({
   datasources: {
     db: {
@@ -12,4 +13,6 @@ const prisma = new PrismaClient({
   },
 });
 
-module.exports = prisma
+
+
+module.exports = prisma;
