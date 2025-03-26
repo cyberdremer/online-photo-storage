c981766e172499691b12781e50d6ad0f98e4f099
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-25 19:02:17 -0700

    Changed top level to use modules

--- back_end/routes/toplevel.js
+++ back_end/routes/toplevel.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
-import signUpRoute from "./signup.js";
-import loginRoute from "./logIn.js";
+const { Router } = require("express");
+const signUpRoute = require("./signup");
+const loginRoute = require("./login");
 const topLevelRoute = Router();
 
 topLevelRoute.use("/signup", signUpRoute);
 topLevelRoute.use("/login", loginRoute);
 
-export default topLevelRoute;
+module.exports = topLevelRoute;
