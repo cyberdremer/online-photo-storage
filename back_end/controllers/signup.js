8c130964ee65ff5a42bfc0ad57ae90a73470acda
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-25 18:41:08 -0700

    Fixed user creation logic

--- back_end/controllers/signup.js
+++ back_end/controllers/signup.js
@@ -1,45 +1,44 @@
-import asyncHandler from "express-async-handler";
-import { validationResult } from "express-validator";
-import { hashPassword } from "../utils/password.js";
-import { signUpValidation } from "../validators/validators.js";
-import prisma from "../prisma-client/prismainstance.js";
+const asyncHandler = require("express-async-handler");
+const { signUpValidation } = require("../validators/validators");
+const hashPassword = require("../utils/password.js");
+const prisma = require("../prisma-client/prismainstance");
+const { validationResult } = require("express-validator");
 
-const createAccountController = asyncHandler(async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({
-      error: {
-        messsage: errors.array(),
-        status: 400,
+const createAccount = [
+  signUpValidation,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: {
+          messsage: errors.array(),
+          status: 400,
+        },
+      });
+    }
+    const hashedpassword = await hashPassword(req.body.password);
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: hashedpassword,
+        folders: {
+          create: {
+            name: "root",
+          }
+        }
+      }
+    });
+
+    return res.status(201).json({
+      data: {
+        messsage: "User has been created!",
+        status: 201,
       },
     });
-  }
-  const hashedpassword = hashPassword(req.body.password);
-  await prisma.user.create({
-    data: {
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedpassword,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      folders: {
-        create: {
-          name: "root",
-          parentfolder: null,
-          parentid: null,
-        },
-      },
-    },
-  });
+  }),
+];
 
-  res.status(201).json({
-    data: {
-      messsage: "User has been created!",
-      status: 201,
-    },
-  });
-});
-
-const createAccount = [signUpValidation, createAccountController];
-
-export { createAccount };
+module.exports = createAccount;
