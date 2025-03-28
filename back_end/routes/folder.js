a31eeb5ded48e7c0298d8797cd25c7837cc97c1a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-28 15:19:30 -0700

    Created folder routes

--- back_end/routes/folder.js
+++ back_end/routes/folder.js
@@ -1,5 +1,16 @@
 const { Router } = require("express");
+const passport = require("../config/passport");
+const {
+  createFolder,
+  getFoldersAndFiles,
+  updateFolder,
+  deleteFolder,
+} = require("../controllers/folder");
 const folderRoute = Router();
 
+folderRoute.post("{/:parentFolderId}", createFolder);
+folderRoute.put("{/:parentFolderId}", updateFolder);
+folderRoute.delete("{/:folderId}", deleteFolder);
+folderRoute.get("{/:folderId}", getFoldersAndFiles);
 
 module.exports = folderRoute;
