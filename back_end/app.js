b9d3f1b3271a6aecb7568e8d3de7af93bfbfcb70
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-30 19:43:24 -0700

    Added error middleware to the server

--- back_end/app.js
+++ back_end/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/error");
 const topLevelRoute = require("./routes/toplevel");
 require("dotenv").config();
 
@@ -6,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use(topLevelRoute);
+app.use(errorHandler);
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
