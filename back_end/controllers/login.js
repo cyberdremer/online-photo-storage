ad145296a775cc43154badf2657ff09f749e5be8
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-30 19:29:09 -0700

    Added custom error handling

--- back_end/controllers/login.js
+++ back_end/controllers/login.js
@@ -4,29 +4,20 @@ const { validationResult } = require("express-validator");
 const { generateUserToken } = require("../utils/token");
 
 const asyncHandler = require("express-async-handler");
+const ErrorWithStatusCode = require("../classes/error");
 
 const loginToAccount = [
   logInValidation,
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        error: {
-          message: errors.array(),
-          status: 400,
-        },
-      });
+      next(new ErrorWithStatusCode(errors.array(), 400));
     }
 
     passport.authenticate("local", async (err, user, info) => {
       if (err) return next(err);
       if (!user) {
-        return res.status(400).json({
-          error: {
-            message: "Error in finding user",
-            status: 400,
-          },
-        });
+        next(new ErrorWithStatusCode("User was not found!", 400));
       }
 
       const token = generateUserToken(user);
