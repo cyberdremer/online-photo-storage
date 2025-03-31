113a2258f81b9695fa3e4aeaf58b4bb4d24bb5d6
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-30 19:24:28 -0700

    Finished the file controller

--- back_end/controllers/file.js
+++ back_end/controllers/file.js
@@ -1,17 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const prisma = require("../prisma-client/prismainstance");
 const {
+  downloadFileCloudinary,
   uploadOnCloudinary,
   deleteOnCloudinary,
 } = require("../utils/cloudinary");
 const upload = require("../middleware/multer");
+const { fileValidation } = require("../validators/validators");
 const decodeFileName = require("../utils/decodefilename");
+const passport = require("passport");
+const { validationResult } = require("express-validator");
+const ErrorWithStatusCode = require("../classes/error");
+const fs = require("fs");
 
 const uploadFile = [
-  upload.single("file"),
+  passport.authenticate("jwt", { session: false }),
+  upload.single("uploadedFile"),
   asyncHandler(async (req, res, next) => {
-    const folderID = req.params.folderID || req.user.rootFolderID;
-    const result = await uploadOnCloudinary(req.file.path);
+    if (!req.file) {
+      next("File not attached, please upload a file!", 413);
+    }
+    const folderID = Number(req.params.folderId) || req.user.folders[0].id;
+    const result = await uploadOnCloudinary(req.file.path, req.user.username);
     await prisma.file.create({
       data: {
         name: decodeFileName(req.file.originalname),
@@ -33,36 +43,125 @@ const uploadFile = [
   }),
 ];
 
-const deleteFile = asyncHandler(async (req, res, next) => {
-  const fileID = req.params.fileID;
-  const fileToDelete = await prisma.file.findFirst({
-    where: {
-      id: fileID,
-      ownerid: req.user.id,
-    },
-  });
+const deleteFile = [
+  passport.authenticate("jwt", { session: false }),
+  asyncHandler(async (req, res, next) => {
+    const fileID = Number(req.params.fileId);
+    const fileToDelete = await prisma.file.findFirst({
+      where: {
+        id: fileID,
+        ownerid: req.user.id,
+      },
+    });
 
-  await deleteOnCloudinary(fileToDelete.cloudinarypublicid);
+    await deleteOnCloudinary(fileToDelete.cloudinarypublicid);
 
-  await prisma.file.delete({
-    where: {
-      id: fileID,
-      ownerid: req.user.id,
-    },
-  });
+    await prisma.file.delete({
+      where: {
+        id: fileID,
+        ownerid: req.user.id,
+      },
+    });
 
-  return res.status(201).json({
-    data: {
-      message: `${fileToDelete.name} has been succesfully deleted!`,
-      status: 201,
-    },
-  });
-});
+    return res.status(201).json({
+      data: {
+        message: `${fileToDelete.name} has been succesfully deleted!`,
+        status: 201,
+      },
+    });
+  }),
+];
 
-const downloadFile = asyncHandler(async (req, res, next) => {});
+const downloadFile = [
+  passport.authenticate("jwt", { session: false }),
+  asyncHandler(async (req, res, next) => {
+    const fileId = Number(req.params.fileId);
+    const fileToDownload = await prisma.file.findFirst({
+      where: {
+        id: fileId,
+        ownerid: req.user.id,
+      },
+    });
+
+    const outputPath = `./public/downloads/${Date.now() + fileToDownload.name}`;
+
+    fs.mkdir(`./public/downloads/`, (err) => {
+      if (err) {
+        console.log(`Directory already exists!`);
+      }
+    });
+
+    const outputStream = await downloadFileCloudinary(
+      fileToDownload.url,
+      outputPath
+    );
+
+    outputStream.on("finish", () => {
+      res.download(outputPath, fileToDownload.name, (err) => {
+        if (err) {
+          next(new ErrorWithStatusCode(err.message, 500));
+        }
+        fs.unlink(outputPath, (fileErr) => {
+          if (fileErr) {
+            console.error("Could not delete file");
+          }
+        });
+      });
+    });
+  }),
+];
+
+const updateFileName = [
+  passport.authenticate("jwt", { session: false }),
+  fileValidation,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      next(new ErrorWithStatusCode(errors.array(), 409));
+    }
+    const fileId = Number(req.params.fileId);
+    const parentId = Number(req.params.folderId) || req.user.folders[0].id;
+
+    const file = await prisma.file.findFirst({
+      where: {
+        folderid: parentId,
+        name: req.body.name,
+        ownerid: req.user.id,
+      },
+    });
+
+    if (file) {
+      next(
+        new ErrorWithStatusCode(
+          "File name is already in use! Please use a unique filename!",
+          400
+        )
+      );
+    }
+
+    const createdFile = await prisma.file.update({
+      where: {
+        ownerid: req.user.id,
+        id: fileId,
+        folderid: parentId,
+      },
+      data: {
+        name: req.body.name,
+      },
+    });
+
+    return res.status(201).json({
+      data: {
+        message: `File name has been updated to: ${createdFile.name}`,
+        status: 201,
+      },
+    });
+  }),
+];
 
 module.exports = {
   uploadFile,
   deleteFile,
   downloadFile,
+  updateFileName,
 };
