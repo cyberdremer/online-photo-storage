b44d5512348fda3432650a91a9041db6c149b5f5
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-30 19:39:55 -0700

    Finished all of the routes

--- back_end/routes/toplevel.js
+++ back_end/routes/toplevel.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const signUpRoute = require("./signup");
 const loginRoute = require("./login");
+const folder = require("./folder");
+const file = require("./file");
 const topLevelRoute = Router();
 
 topLevelRoute.use("/signup", signUpRoute);
 topLevelRoute.use("/login", loginRoute);
+topLevelRoute.use("/folder", folder);
+topLevelRoute.use("/file", file)
 
 module.exports = topLevelRoute;
