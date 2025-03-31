3b0769e97d4918c6a2348600619c34a8210f2f16
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-03-30 19:38:16 -0700

    Added all controllers to the file route

--- back_end/routes/file.js
+++ back_end/routes/file.js
@@ -1,4 +1,16 @@
 const { Router } = require("express");
 const fileRoute = Router();
+const {
+  uploadFile,
+  deleteFile,
+  downloadFile,
+  updateFileName,
+} = require("../controllers/file");
+
+fileRoute.delete("/:fileId", deleteFile);
+fileRoute.get("/:fileId", downloadFile);
+fileRoute.put("/:fileId", updateFileName);
+fileRoute.post("/:folderId?", uploadFile);
+
 
 module.exports = fileRoute;
