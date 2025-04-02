5649b6c07a6e3431f402294a888b65ad3a57849f
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-02 15:01:17 -0700

    Added cors to express server

--- back_end/app.js
+++ back_end/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const errorHandler = require("./middleware/error");
 const topLevelRoute = require("./routes/toplevel");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(topLevelRoute);
 app.use(errorHandler);
