39c6700b233c860d37e444cdc560ae240b27d15a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-02 17:08:59 -0700

    Created refresh token controller

---
+++ back_end/controllers/token.js
@@ -0,0 +1,33 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const ErrorWithStatusCode = require("../classes/error");
+const { generateUserToken, generateRefreshToken } = require("../utils/token");
+
+const tokenRefresh = asyncHandler(async (req, res, next) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    next(new ErrorWithStatusCode("Token Invalid or Expired", 403));
+  }
+
+  jwt.verify(refreshToken, process.env.JWTREFRESHTOKENSECRET, (err, user) => {
+    if (err) {
+      next(new ErrorWithStatusCode("Token Invalid or Expired", 403));
+    }
+    const newUserToken = generateUserToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    });
+    res.json({
+      data: {
+        token: newUserToken,
+        message: "Your token has been refreshed",
+      },
+    });
+  });
+});
+
+
+module.exports = tokenRefresh;
