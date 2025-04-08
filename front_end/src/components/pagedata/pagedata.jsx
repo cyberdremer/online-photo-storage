a577a107d87b2d2d71dc69011c4177c44e181c83
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-07 20:36:57 -0700

    Created page route data

--- front_end/src/components/pagedata/pagedata.jsx
+++ front_end/src/components/pagedata/pagedata.jsx
@@ -1,6 +1,9 @@
 import SignUp from "../pages/signup";
 import Landing from "../pages/landing";
 import Login from "../pages/login";
+import AssetDashboard from "../pages/assetdashboard";
+import RequireAuth from "../protection/protectedroute";
+import LogOut from "../pages/logout";
 
 const pageData = [
   {
@@ -17,6 +20,20 @@ const pageData = [
     path: "/login",
     element: <Login></Login>,
     title: "Log In Page",
+  },
+  {
+    path: "/view-assets",
+    element: (
+      <RequireAuth>
+        <AssetDashboard></AssetDashboard>
+      </RequireAuth>
+    ),
+    title: "Dave.Save",
+  },
+  {
+    path: "/logout",
+    element: <LogOut></LogOut>,
+    title: "Log Out Page",
   },
 ];
 
