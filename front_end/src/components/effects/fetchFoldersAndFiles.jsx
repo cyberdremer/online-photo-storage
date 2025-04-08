508c5039ce6b0e92b134583b4728b114d8cefcff
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-07 20:44:24 -0700

    Implemented Authentication Context

---
+++ front_end/src/components/effects/fetchFoldersAndFiles.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useContext, useState } from "react";
+import { AuthContext } from "../context/auth";
+const useFetchAssets = (folderId) => {
+  const { cookie } = useContext(AuthContext);
+  const [folderUrl, setFolderUrl] = useState("http://localhost:4000/folder");
+  const [items, setItems] = useState({
+    folders: [],
+    files: [],
+  });
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${folderUrl + folderId}`, {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${cookie.usertoken}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const foldersAndFiles = await response.json();
+        setItems({
+          ...items,
+          folders: foldersAndFiles.data.folders,
+          files: foldersAndFiles.data.files,
+        });
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [folderUrl, folderId]);
+
+  return { items, loading, error, setItems };
+};
+
+export default useFetchAssets;
