e1ff6adef14735e72382e33fc19c9151f82f0967
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-07 20:48:22 -0700

    Finished Landing Component

--- front_end/src/components/pages/landing.jsx
+++ front_end/src/components/pages/landing.jsx
@@ -5,7 +5,7 @@ import { Flex } from "@chakra-ui/react";
 const Landing = () => {
   return (
     <>
-      <Flex grow="1" direction="column" padding="1rem">
+      <Flex grow="1" direction="column" minHeight="100%">
         <Header></Header>
         <LandingBlurb></LandingBlurb>
         <Footer></Footer>
