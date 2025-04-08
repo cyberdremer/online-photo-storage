a7c61b1d13a974c0e70eae4fafdce8555334a28c
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-07 20:30:50 -0700

    Created generic modal that allows for any component to be rendered inside of it

---
+++ front_end/src/components/fragments/modals/modals.jsx
@@ -0,0 +1,24 @@
+import { Box, CloseButton, Dialog, FileUpload, Portal } from "@chakra-ui/react";
+
+const GenericModal = ({ isOpen, children, title, setOpen }) => {
+  return (
+    <Dialog.Root open={isOpen} >
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>{title}</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body>{children}</Dialog.Body>
+            <Dialog.CloseTrigger asChild>
+              <CloseButton onClick={() => setOpen(!isOpen)} size="sm" />
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
+
+export default GenericModal;
