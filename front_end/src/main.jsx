3c19e2d695b9e87d2503cd52ed9fb3db203ea257
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-08 11:54:40 -0700

    Finished main app configuration

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
+import { CookiesProvider } from "react-cookie";
+import { AuthProvider } from "./components/context/auth.jsx";
+import { UserProvider } from "./components/context/userinfo.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App />
+      <CookiesProvider>
+        <AuthProvider>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </AuthProvider>
+      </CookiesProvider>
     </Provider>
   </StrictMode>
 );
