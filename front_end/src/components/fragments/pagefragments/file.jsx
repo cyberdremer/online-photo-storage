f01189013da33b69c915314e36bf3c9329495e5b
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-07 21:21:11 -0700

    Implemented component to display file info

---
+++ front_end/src/components/fragments/pagefragments/file.jsx
@@ -0,0 +1,102 @@
+import {
+  GridItem,
+  Stack,
+  FormatByte,
+  IconButton,
+  Menu,
+  Portal,
+  Button,
+  Box,
+  Text,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+import { FaFile } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useRef, useState } from "react";
+import { DeleteAlert } from "./alerts/alerts";
+const File = ({
+  name,
+  createdat,
+  size,
+  updatedat,
+  handleDelete,
+  handleDownload,
+  handleRename,
+  id,
+  folderid
+}) => {
+  const [infoVisible, setInfoVisible] = useState(false);
+
+  const displayFileExtraInfo = () => {
+    setInfoVisible(!infoVisible);
+  };
+
+  return (
+    <GridItem id={id}>
+      <Box maxW="sm" borderWidth="1px" minH="3rem">
+        <Box p="4" spaceY="2">
+          <HStack>
+            <VStack alignItems="flex-start" textStyle="sm">
+              <FaFile size="5rem"></FaFile>
+              <Text>{name}</Text>
+              <Text>
+                Size: <FormatByte value={size}></FormatByte>
+              </Text>
+              {infoVisible && (
+                <>
+                  <Text>Created At: {createdat}</Text>
+
+                  <Text>Updated At: {updatedat}</Text>
+                </>
+              )}
+            </VStack>
+            <Menu.Root>
+              <Menu.Trigger asChild>
+                <IconButton>
+                  <BsThreeDotsVertical></BsThreeDotsVertical>
+                </IconButton>
+              </Menu.Trigger>
+              <Portal>
+                <Menu.Positioner>
+                  <Menu.Content>
+                    <Menu.Item
+                      value="delete-file"
+                      onClick={handleDelete}
+                      id={ id}
+                    >
+                      Delete File
+                    </Menu.Item>
+
+                    <Menu.Item
+                      value="update-file"
+                      onClick={handleRename}
+                      id={ id}
+                    >
+                      Update File Name
+                    </Menu.Item>
+                    <Menu.Item
+                      value="download-file"
+                      onClick={handleDownload}
+                      id={ id}
+                    >
+                      Download File
+                    </Menu.Item>
+                    <Menu.Item
+                      value="display-file-infop"
+                      onClick={displayFileExtraInfo}
+                    >
+                      {infoVisible ? "Hide File Info" : "Display File Info"}
+                    </Menu.Item>
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Portal>
+            </Menu.Root>
+          </HStack>
+        </Box>
+      </Box>
+    </GridItem>
+  );
+};
+
+export default File;
