3f97a670294c6d61fc36b1435a6575acbeb9a8f2
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-08 23:22:39 -0700

    Implemented remaining functionality for CRUD operations

--- front_end/src/components/pages/assetdashboard.jsx
+++ front_end/src/components/pages/assetdashboard.jsx
@@ -2,7 +2,7 @@ import { Flex, Portal } from "@chakra-ui/react";
 import Header from "../fragments/pagefragments/header";
 import AssetDisplay from "./assetdisplay";
 import Footer from "../fragments/pagefragments/footer";
-import { useContext, useRef, useState } from "react";
+import { useContext, useReducer, useRef, useState } from "react";
 import VaultHeader from "../fragments/pagefragments/vaultheader";
 import { UserContext } from "../context/userinfo";
 import useFetchAssets from "../effects/fetchFoldersAndFiles";
@@ -11,8 +11,9 @@ import {
   deleteFile,
   deleteFolder,
   downloadFile,
+  formPostRequest,
   renameFile,
-  updateFolder,
+  renameFolder,
 } from "../utils/requests";
 import { AuthContext } from "../context/auth";
 import {
@@ -24,12 +25,23 @@ import GenericModal from "../fragments/modals/modals";
 import FileDisplay from "../fragments/displays/filedisplay";
 import FileUploadBox from "../fragments/forms/fileupload";
 import FolderDisplay from "../fragments/displays/folderdisplay";
-import FolderResourceForm from "../fragments/forms/resourceform";
+import ResourceForm from "../fragments/forms/resourceform";
+import ModalReducer from "../reducers/modalreducer";
+
+const initialModalState = {
+  fileDelete: false,
+  fileUpload: false,
+  fileRename: false,
+  folderDelete: false,
+  folderRename: false,
+  folderCreate: false,
+};
 
 const AssetDashboard = () => {
   const { user, updateCurrentFolder } = useContext(UserContext);
   const { cookie } = useContext(AuthContext);
-  const fileRef = useRef(null);
+  const currentlySelectedFileRef = useRef(null);
+  const currentlySelectedFolderRef = useRef(null);
   const { items, loading, error, setItems } = useFetchAssets(
     user.currentFolderId
   );
@@ -38,17 +50,11 @@ const AssetDashboard = () => {
   // as the replacing criteria.
 
   const [openDelete, setOpenDelete] = useState(false);
-  const [modalVisible, setModalVisible] = useState({
-    fileDelete: false,
-    fileUpload: false,
-    fileRename: false,
-    folderDelete: false,
-    folderRename: false,
-    folderCreate: false,
-  });
+
+  const [modalVisible, dispatch] = useReducer(ModalReducer, initialModalState);
 
   const [requestError, setRequestError] = useState({
-    status: false.value,
+    status: false,
     message: "",
   });
 
@@ -58,49 +64,36 @@ const AssetDashboard = () => {
   });
 
   const changeFileDeleteVisbility = (e) => {
-    const { id } = e.target.id;
-    fileRef.current = id;
-    setModalVisible({
-      ...modalVisible,
-      fileDelete: !modalVisible.fileDelete,
-    });
+    currentlySelectedFileRef.current = e.target.id;
+    dispatch({ type: "displayFileDelete" });
+  };
+  const changeFileRenameVisbility = (e) => {
+    currentlySelectedFileRef.current = e.target.id;
+    dispatch({ type: "displayFileRename" });
+  };
+  const changeFileUploadVisbility = (e) => {
+    dispatch({ type: "displayFileUpload" });
   };
 
-  const changeFileUploadVisbility = () => {
-    setModalVisible({
-      ...modalVisible,
-      fileUpload: !modalVisible.fileUpload,
-    });
+  const changeFolderDeleteVisbility = (e) => {
+    currentlySelectedFolderRef.current = e.target.id;
+    dispatch({ type: "displayFolderDelete" });
   };
-  const changeFileRenameVisbility = () => {
-    
-    setModalVisible({
-      ...modalVisible,
-      fileRename: !modalVisible.fileRename,
-    });
+  const changeFolderRenameVisbility = (e) => {
+    currentlySelectedFolderRef.current = e.target.id;
+    dispatch({ type: "displayFolderRename" });
   };
-  const changeFolderDeleteVisbility = () => {
-    setModalVisible({
-      ...modalVisible,
-      folderDelete: !modalVisible.folderDelete,
-    });
-  };
-  const changeFolderRenameVisbility = () => {
-    setModalVisible({
-      ...modalVisible,
-      folderRename: !modalVisible.folderRename,
-    });
-  };
-  const changeFolderCreateVisbility = () => {
-    setModalVisible({
-      ...modalVisible,
-      folderCreate: !modalVisible.folderCreate,
-    });
+  const changeFolderCreateVisbility = (e) => {
+    dispatch({ type: "displayFolderCreate" });
   };
 
   const handleFileRename = async (form) => {
     try {
-      const response = await renameFile(ref.currentId, form, cookie.usertoken);
+      const response = await renameFile(
+        currentlySelectedFileRef.current,
+        form,
+        cookie.usertoken
+      );
       if (response.error) {
         throw new Error(response.error.message);
       }
@@ -115,10 +108,11 @@ const AssetDashboard = () => {
         });
       }, 10000);
       const newFiles = items.files.map((file) => {
-        if (file.id === fileRef.current) {
+        if (file.id === Number(currentlySelectedFileRef.current)) {
           return {
             ...file,
             name: form.name,
+            updatedat: response.data.fileInfo.updatedat,
           };
         }
         return file;
@@ -140,7 +134,10 @@ const AssetDashboard = () => {
 
   const handleFileDelete = async () => {
     try {
-      const response = await deleteFile(fileRef.current, cookie.usertoken);
+      const response = await deleteFile(
+        currentlySelectedFileRef.current,
+        cookie.usertoken
+      );
       if (response.error) {
         throw new Error(response.error.message);
       }
@@ -156,13 +153,15 @@ const AssetDashboard = () => {
       }, 10000);
 
       const newFiles = items.files.filter(
-        (file) => file.id !== fileRef.current
+        (file) => file.id !== Number(currentlySelectedFileRef.current)
       );
       setItems({
         ...items,
         files: newFiles,
       });
+      dispatch({ type: "displayFileDelete" });
     } catch (error) {
+      dispatch({ type: "displayFileDelete" });
       setRequestError({
         status: true,
         message: error.message,
@@ -208,16 +207,91 @@ const AssetDashboard = () => {
     }
   };
 
-  const handleFileUpload = async () => {};
+  const handleFileUpload = async () => {
+    try {
+    } catch (err) {}
+  };
 
   const handleFolderChange = (e) => {
     const folderId = e.target.id;
     updateCurrentFolder(folderId);
   };
-  const handleFolderRename = (folderId, name) => {};
+
+  const handleFolderCreate = async (form, parentId) => {
+    try {
+      const response = await formPostRequest(
+        form,
+        `http://localhost:4000/folder/${parentId}`
+      );
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+
+      const newFolderArray = [
+        ...items.files,
+        {
+          name: response.data.folderInfo.name,
+          createdat: response.data.folderInfo.createdat,
+          updatedat: response.data.folderInfo.updatedat,
+          id: response.data.folderInfo.id,
+        },
+      ];
+      setItems({ ...items, files: newFolderArray });
+    } catch (err) {}
+  };
+  const handleFolderRename = async (name) => {
+    try {
+      const response = await renameFolder(
+        currentlySelectedFolderRef.current,
+        name,
+        cookie.usertoken
+      );
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+
+      setRequestSuccess({
+        status: true,
+        message: response.data.message,
+      });
+
+      setTimeout(() => {
+        setRequestSuccess({
+          status: false,
+          message: "",
+        });
+      });
+
+      const newFolderArray = items.folders.map((folder) => {
+        if (folder.id === Number(currentlySelectedFolderRef.current)) {
+          return {
+            ...items.folders,
+            name: form.name,
+            updatedat: response.data.folderInfo.updatedat,
+          };
+        }
+        return file;
+      });
+    } catch (error) {
+      setRequestError({
+        status: true,
+        message: error.message,
+      });
+
+      setTimeout(() => {
+        setRequestError({
+          status: false,
+          message: "",
+        });
+      }, 10000);
+    }
+  };
   const handleFolderDelete = async (folderId) => {
     try {
-      const response = await deleteFolder(folderId, cookie.usertoken);
+      const response = await deleteFolder(
+        currentlySelectedFolderRef.current,
+        cookie.usertoken
+      );
       if (response.error) {
         throw new Error(response.error.message);
       }
@@ -259,16 +333,10 @@ const AssetDashboard = () => {
         <Header></Header>
         <Portal>
           {requestError.status && (
-            <ErrorAlert
-              status={requestError.status}
-              message={requestError.message}
-            ></ErrorAlert>
+            <ErrorAlert message={requestError.message}></ErrorAlert>
           )}
           {requestSuccess.status && (
-            <SuccessAlert
-              status={requestSuccess.status}
-              message={requestSuccess.message}
-            ></SuccessAlert>
+            <SuccessAlert message={requestSuccess.message}></SuccessAlert>
           )}
         </Portal>
         <VaultHeader
@@ -301,14 +369,14 @@ const AssetDashboard = () => {
           title={"Rename File"}
           setOpen={changeFileRenameVisbility}
         >
-          <FolderResourceForm
+          <ResourceForm
             success={requestSuccess.status}
             successMessage={requestSuccess.message}
             error={requestError.status}
             errorMessage={requestError.message}
             handleSubmission={() => {}}
             name={"New File Name: "}
-          ></FolderResourceForm>
+          ></ResourceForm>
         </GenericModal>
 
         <GenericModal
@@ -316,14 +384,14 @@ const AssetDashboard = () => {
           title={"Create Folder"}
           setOpen={changeFolderCreateVisbility}
         >
-          <FolderResourceForm
+          <ResourceForm
             success={requestSuccess.status}
             successMessage={requestSuccess.message}
             error={requestError.status}
             errorMessage={requestError.message}
             handleSubmission={() => {}}
             name={"Folder Name: "}
-          ></FolderResourceForm>
+          ></ResourceForm>
         </GenericModal>
 
         <GenericModal
@@ -331,19 +399,19 @@ const AssetDashboard = () => {
           title={"Rename Folder"}
           setOpen={changeFolderRenameVisbility}
         >
-          <FolderResourceForm
-          success={requestSuccess.status}
-          successMessage={requestSuccess.message}
-          error={requestError.status}
-          errorMessage={requestError.message}
-          handleSubmission={() => {}}
-          name={"New Folder Name:"}
-          ></FolderResourceForm>
+          <ResourceForm
+            success={requestSuccess.status}
+            successMessage={requestSuccess.message}
+            error={requestError.status}
+            errorMessage={requestError.message}
+            handleSubmission={() => {}}
+            name={"New Folder Name:"}
+          ></ResourceForm>
         </GenericModal>
 
         <DeleteAlert
           deleteTitle="Delete Folder"
-          assetType={"file"}
+          assetType={"folder"}
           open={modalVisible.folderDelete}
           deleteAction={() => {}}
           handleClose={changeFolderDeleteVisbility}
