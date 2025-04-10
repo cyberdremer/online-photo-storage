b82bf5c37fb1ae83508f0057a8dafd6ff6c8a09e
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-10 11:07:40 -0700

    Added limit to express middleware

--- back_end/app.js
+++ back_end/app.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({limit: '10mb', extended: true }));
 app.use(cors());
 
 app.use(topLevelRoute);
