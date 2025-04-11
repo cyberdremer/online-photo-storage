23d1c36b2955628739fe167b648fb2019f41cfa4
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-10 23:51:32 -0700

    Updated vault header to contain the folder breadcrumbs

--- front_end/src/components/fragments/pagefragments/vaultheader.jsx
+++ front_end/src/components/fragments/pagefragments/vaultheader.jsx
@@ -1,16 +1,28 @@
-import { HStack, Group, IconButton, Icon, Heading } from "@chakra-ui/react";
+import {
+  HStack,
+  Group,
+  IconButton,
+  Icon,
+  Heading,
+  Breadcrumb,
+} from "@chakra-ui/react";
 import { useContext } from "react";
 import { FaFolderPlus, FaFileUpload, FaSadCry } from "react-icons/fa";
 import { UserContext } from "../../context/userinfo";
+import { Link } from "react-router-dom";
+import FolderBreadCrumbs from "./folderbreadcrumbs";
 
-const VaultHeader = ({ displayFileModal, displayFolderModal }) => {
-  const { user } = useContext(UserContext);
+const VaultHeader = ({
+  displayFileModal,
+  displayFolderModal,
+  folderHistory,
+  updateFolderHistory,
+}) => {
+
   return (
     <>
-      <HStack justify="space-between">
-        <Icon>
-          <Heading>{user.currentFolderName}</Heading>
-        </Icon>
+      <HStack justifyContent="space-between" alignItems="center" padding="1rem">
+        <FolderBreadCrumbs folderHistory={folderHistory} updateFolderHistory={updateFolderHistory}></FolderBreadCrumbs>
         <Group gap="1rem">
           <IconButton onClick={displayFolderModal}>
             <FaFolderPlus></FaFolderPlus>
