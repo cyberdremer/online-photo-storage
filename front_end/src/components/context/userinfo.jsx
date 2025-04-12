cb88521d5a0ba8655978d9f3a526e72401a0cd6a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-12 16:44:47 -0700

    Final iteration for userinfo context

---
+++ front_end/src/components/context/userinfo.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    username: "",
+    rootFolderId: null,
+    rootFolderName: "root",
+    currentFolderId: null,
+    currentFolderName: "root",
+  });
+
+  const initUser = (user) => {
+    setUser({
+      ...user,
+      username: user.username,
+      rootFolderId: user.rootFolderId,
+      currentFolderId: user.rootFolderId,
+      currentFolderName: "root"
+    });
+  };
+
+
+
+  const updateCurrentFolder = (currentFolderId, currentFolderName) => {
+    const sanitizedFolderId = user.rootFolderId === currentFolderId ? "" : currentFolderId;
+    setUser({
+      ...user,
+      currentFolderId: sanitizedFolderId,
+      currentFolderName: currentFolderName
+    });
+  };
+
+  return (
+    <UserContext.Provider value={{ user, initUser, updateCurrentFolder }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider, UserContext };
