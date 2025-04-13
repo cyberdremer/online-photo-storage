e5ed85033bb55cfd8cbd7f0a4bd0316e5661e14b
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-12 19:00:01 -0700

    Added font import to main app

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "@/components/ui/provider";
 import { CookiesProvider } from "react-cookie";
 import { AuthProvider } from "./components/context/auth.jsx";
 import { UserProvider } from "./components/context/userinfo.jsx";
+import "@fontsource-variable/inter/index.css"
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
