5a62a210820b3bf7c3075aee8731bc16f78c89a3
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-12 19:33:57 -0700

    Need to implement way to bypass the login screen if the users token is valid

---
+++ front_end/src/components/effects/checkLogin.jsx
@@ -0,0 +1,11 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/auth";
+
+const CheckTokenExpiry = () => {
+  const { cookie } = useContext(AuthContext);
+
+  useEffect(() => {
+    
+  })
+
+};
