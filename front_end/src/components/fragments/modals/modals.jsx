42975c64ae71133f398993fcab04b5afe8a6562e
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-13 19:50:47 -0700

    Updated modal to have styling

--- front_end/src/components/fragments/modals/modals.jsx
+++ front_end/src/components/fragments/modals/modals.jsx
@@ -1,12 +1,14 @@
+import ModeColors from "@/components/utils/modecolors";
 import { Box, CloseButton, Dialog, FileUpload, Portal } from "@chakra-ui/react";
 
 const GenericModal = ({ open, children, title, handleClose }) => {
+  const{secondary, primary, buttonText, buttonBackground} = ModeColors();
   return (
     <Dialog.Root open={open} motionPreset="slide-in-top" >
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
-          <Dialog.Content>
+          <Dialog.Content backgroundColor={primary}>
             <Dialog.Header>
               <Dialog.Title>{title}</Dialog.Title>
             </Dialog.Header>
