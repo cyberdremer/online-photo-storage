8b81850afb9ef8a3f1f75ba2e5742d6e51816659
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-13 19:47:45 -0700

    Added styling to resource form

--- front_end/src/components/fragments/forms/resourceform.jsx
+++ front_end/src/components/fragments/forms/resourceform.jsx
@@ -1,6 +1,7 @@
-import { Fieldset, Field, Input, Button, Portal } from "@chakra-ui/react";
+import { Fieldset, Field, Input, Button, Portal, Text } from "@chakra-ui/react";
 import { SuccessAlert, ErrorAlert } from "../alerts/alerts";
 import { useState } from "react";
+import ModeColors from "@/components/utils/modecolors";
 
 const ResourceForm = ({
   successMessage,
@@ -20,21 +21,39 @@ const ResourceForm = ({
     setResourceName({ ...resourceName, name: input });
   };
 
+  const {
+    primary,
+    secondary,
+    primaryText,
+    secondaryText,
+    inputfieldColors,
+    buttonText,
+    buttonBackground,
+  } = ModeColors();
+
   return (
     <Fieldset.Root>
       <Portal>
         {success && <SuccessAlert message={successMessage}></SuccessAlert>}
         {error && <ErrorAlert message={errorMessage}></ErrorAlert>}
       </Portal>
-      <Fieldset.Legend>{title}</Fieldset.Legend>
+      <Fieldset.Legend color={primaryText}>{title}</Fieldset.Legend>
       <Fieldset.Content>
         <Field.Root>
-          <Field.Label>{name}</Field.Label>
-          <Input name="name" onChange={handleChange}></Input>
+          <Field.Label color={primaryText}>{name}</Field.Label>
+          <Input
+            name="name"
+            onChange={handleChange}
+            backgroundColor={inputfieldColors}
+          ></Input>
         </Field.Root>
       </Fieldset.Content>
-      <Button type="submit" onClick={() => handleSubmission(resourceName)}>
-        Create
+      <Button
+        type="submit"
+        color={buttonBackground}
+        onClick={() => handleSubmission(resourceName)}
+      >
+        <Text color={buttonText}>Create</Text>
       </Button>
     </Fieldset.Root>
   );
