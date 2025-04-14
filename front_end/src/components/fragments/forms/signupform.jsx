63d7d2ca75fdb16839113e10f0402230f13507bf
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-13 19:48:01 -0700

    Added styling to sign up form

--- front_end/src/components/fragments/forms/signupform.jsx
+++ front_end/src/components/fragments/forms/signupform.jsx
@@ -1,8 +1,10 @@
-import { Stack, Field, Input, Button, Fieldset, Alert } from "@chakra-ui/react";
+import { Stack, Field, Input, Button, Fieldset, Alert, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { formPostRequest } from "../../utils/requests";
+import ModeColors from "@/components/utils/modecolors";
 
 const SignUpForm = () => {
+  const{secondary, primary, primaryText, secondaryText, buttonText, buttonBackground ,inputfieldColors} = ModeColors();
   const [createAccount, setCreateAccount] = useState({
     message: "",
     accountMade: false,
@@ -61,7 +63,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <Stack gap="4" alignSelf={"center"} maxW="lg" style={{paddingBottom: "2rem"}}>
+    <Stack gap="4" alignSelf={"center"} minW="100%" style={{paddingBottom: "2rem"}} backgroundColor={secondary}>
       {error.errorBool && (
         <Alert.Root status="error">
           <Alert.Indicator />
@@ -74,25 +76,26 @@ const SignUpForm = () => {
           <Alert.Title>{createAccount.message}</Alert.Title>
         </Alert.Root>
       )}
-      <Fieldset.Root animationName="fade-in" animationDuration="slowest">
+      <Fieldset.Root animationName="fade-in" animationDuration="slowest" alignItems="center" paddingTop="4rem">
         <Stack gap="4" maxW="lg">
-          <Fieldset.Legend fontSize="4xl">Account Details</Fieldset.Legend>
-          <Fieldset.HelperText fontSize="xl">
+          <Fieldset.Legend fontSize="4xl" color={primaryText}>Account Details</Fieldset.Legend>
+          <Fieldset.HelperText fontSize="xl" color={secondaryText}>
             Please enter your details below to create an account on Dave.Save
           </Fieldset.HelperText>
           <Stack gap="2rem">
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 Username <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
               <Input
                 variant="subtle"
                 onChange={handleChange}
                 name="username"
+                backgroundColor={inputfieldColors}
               ></Input>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 Email <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
               <Input
@@ -100,30 +103,33 @@ const SignUpForm = () => {
                 onChange={handleChange}
                 name="email"
                 type="email"
+                backgroundColor={inputfieldColors}
               ></Input>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 First Name <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
               <Input
                 variant="subtle"
                 onChange={handleChange}
                 name="firstname"
+                backgroundColor={inputfieldColors}
               ></Input>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 Last Name <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
               <Input
                 variant="subtle"
                 onChange={handleChange}
                 name="lastname"
+                backgroundColor={inputfieldColors}
               ></Input>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 Password <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
               <Input
@@ -131,15 +137,16 @@ const SignUpForm = () => {
                 onChange={handleChange}
                 name="password"
                 type="password"
+                backgroundColor={inputfieldColors}
               ></Input>
-              <Fieldset.HelperText>
+              <Fieldset.HelperText color={secondaryText}>
                 Password must be at least 8 characters long, contain an uppercase
                 letter, a lowercase letter, a number, and a special character.
                 
               </Fieldset.HelperText>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>
+              <Field.Label color={primaryText}>
                 Confirm Password{" "}
                 <Field.RequiredIndicator></Field.RequiredIndicator>
               </Field.Label>
@@ -148,11 +155,12 @@ const SignUpForm = () => {
                 onChange={handleChange}
                 name="confirmpassword"
                 type="password"
+                backgroundColor={inputfieldColors}
               ></Input>
             </Field.Root>
           </Stack>
-          <Button onClick={onSubmit} size="sm" alignSelf="center" width="100%">
-            Sign Up.
+          <Button onClick={onSubmit} size="sm" alignSelf="center" width="100%" color={buttonBackground}>
+            <Text color={buttonText}>Sign Up.</Text>
           </Button>
         </Stack>
       </Fieldset.Root>
