157f811228b38cdaff0a2eacafb3041b1c6e2b7b
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-13 19:39:18 -0700

    Added different colors based on the color mode

--- front_end/src/theme.jsx
+++ front_end/src/theme.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { createSystem } from "@chakra-ui/react";
 import { defineConfig } from "@chakra-ui/react";
 import { defaultConfig } from "@chakra-ui/react";
@@ -9,6 +10,30 @@ const interConfig = defineConfig({
       fonts: {
         heading: { value: "Inter Variable" },
         body: { value: "Inter Variable" },
+      },
+      colors: {
+        dark: {
+          primary: { value: "#1A202C" },
+          secondary: { value: "#2D3748" },
+          accent: { value: "#3182CE" },
+          primarytext: { value: "#E5E5E5" },
+          secondarytext: { value: "" },
+          buttontext: { value: "#000000" },
+          buttonbackground: { value: "#FFFFFF" },
+          borders: { value: "#4A5568" },
+          inputfields: { value: "#2D3748" },
+        },
+        light: {
+          primary: { value: "#F7FAFC" },
+          secondary: { value: "#E2E8F0" },
+          accent: { value: "#3182CE" },
+          text: { value: "#2D3748" },
+          secondarytext: { value: "#4A5568" },
+          buttontext: { value: "#FFFFFF" },
+          buttonbackground: { value: "#FFFFFF" },
+          borders: { value: "#CBD5E0" },
+          inputfields: { value: "#FFFFFF" },
+        },
       },
     },
   },
