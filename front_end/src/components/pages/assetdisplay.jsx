629f7f9b56b408947233e8df9b6e718294983263
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-13 19:44:06 -0700

    Added styling to the various views

--- front_end/src/components/pages/assetdisplay.jsx
+++ front_end/src/components/pages/assetdisplay.jsx
@@ -10,16 +10,26 @@ import {
   VStack,
   Icon,
   Text,
-  Highlight
+  Highlight,
 } from "@chakra-ui/react";
 import { BsFolder2Open } from "react-icons/bs";
 
 import { FaFolderOpen } from "react-icons/fa";
+import ModeColors from "../utils/modecolors";
 
 const AssetDisplay = ({ children, loading, error, items }) => {
+  const { secondary, secondaryText } = ModeColors();
+
   if (loading) {
     return (
-      <Stack alignSelf="center" flex="1" animationName="pulse" animationDuration="slowest">
+      <Stack
+        alignSelf="center"
+        flex="1"
+        animationName="pulse"
+        animationDuration="slowest"
+        backgroundColor={secondary}
+        minW="100%"
+      >
         <ProgressCircle.Root value={null} size="xl">
           <ProgressCircle.Circle>
             <ProgressCircle.Track></ProgressCircle.Track>
@@ -41,24 +51,50 @@ const AssetDisplay = ({ children, loading, error, items }) => {
 
   if (items && items.files.length === 0 && items.folders.length === 0) {
     return (
-      <Stack alignSelf="center" padding="1rem" animationName="fade-in" animationDuration="slowest" flex="1">
-        <Icon alignSelf="center">
+      <Stack
+        alignSelf="center"
+        padding="1rem"
+        flex="1"
+        backgroundColor={secondary}
+        minW="100%"
+        alignItems="center"
+        justifyItems="center"
+        gap={20}
+        paddingTop={10}
+        shadow={"2xl"}
+      >
+        <Icon
+          alignSelf="center"
+          animationName="fade-in"
+          animationDuration="slowest"
+        >
           <BsFolder2Open size="10rem"></BsFolder2Open>
         </Icon>
-        
-          <Text fontSize="xl" lineClamp="4">
-            <Highlight query={["no", "creating", "uploading"]} styles={{fontWeight:"semibold"}}>
-            There are no assets here! :( You can make it less lonely here by uploading a
-            file or creating a folder!
-            </Highlight>
-            
-          </Text>
-        
+
+        <Text
+          fontSize="xl"
+          lineClamp="4"
+          animationName="fade-in"
+          animationDuration="slowest"
+          color={secondaryText}
+        >
+          <Highlight
+            query={["no", "creating", "uploading"]}
+            styles={{ fontWeight: "semibold" }}
+          >
+            There are no assets here! :( You can make it less lonely here by
+            uploading a file or creating a folder!
+          </Highlight>
+        </Text>
       </Stack>
     );
   }
 
-  return <Container flex="1">{children}</Container>;
+  return (
+    <Container flex="1" backgroundColor={secondary} minW="100%" paddingTop={10} shadow="lg">
+      {children}
+    </Container>
+  );
 };
 
 export default AssetDisplay;
