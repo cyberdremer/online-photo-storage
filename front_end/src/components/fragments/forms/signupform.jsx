e358b423529e43c46519e0c47f7762c9c110ac26
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-14 18:51:38 -0700

    Fixed positioning of alerts

--- front_end/src/components/fragments/forms/signupform.jsx
+++ front_end/src/components/fragments/forms/signupform.jsx
@@ -1,10 +1,28 @@
-import { Stack, Field, Input, Button, Fieldset, Alert, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Stack,
+  Field,
+  Input,
+  Button,
+  Fieldset,
+  Alert,
+  Text,
+  Portal,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { formPostRequest } from "../../utils/requests";
 import ModeColors from "@/components/utils/modecolors";
+import { ErrorAlert, SuccessAlert } from "../alerts/alerts";
 
 const SignUpForm = () => {
-  const{secondary, primary, primaryText, secondaryText, buttonText, buttonBackground ,inputfieldColors} = ModeColors();
+  const containerRef = useRef();
+  const {
+    secondary,
+    primaryText,
+    secondaryText,
+    buttonText,
+    buttonBackground,
+    inputfieldColors,
+  } = ModeColors();
   const [createAccount, setCreateAccount] = useState({
     message: "",
     accountMade: false,
@@ -58,113 +76,127 @@ const SignUpForm = () => {
           errorBool: false,
           message: "",
         });
-      });
+      }, 10000);
     }
   };
 
   return (
-    <Stack gap="4" alignSelf={"center"} minW="100%" style={{paddingBottom: "2rem"}} backgroundColor={secondary}>
-      {error.errorBool && (
-        <Alert.Root status="error">
-          <Alert.Indicator />
-          <Alert.Title>{error.message}</Alert.Title>
-        </Alert.Root>
-      )}
+    <>
+      {error.errorBool && <ErrorAlert message={error.message}></ErrorAlert>}
       {createAccount.accountMade && (
-        <Alert.Root status="success">
-          <Alert.Indicator />
-          <Alert.Title>{createAccount.message}</Alert.Title>
-        </Alert.Root>
+        <SuccessAlert message={createAccount.message}></SuccessAlert>
       )}
-      <Fieldset.Root animationName="fade-in" animationDuration="slowest" alignItems="center" paddingTop="4rem">
-        <Stack gap="4" maxW="lg">
-          <Fieldset.Legend fontSize="4xl" color={primaryText}>Account Details</Fieldset.Legend>
-          <Fieldset.HelperText fontSize="xl" color={secondaryText}>
-            Please enter your details below to create an account on Dave.Save
-          </Fieldset.HelperText>
-          <Stack gap="2rem">
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                Username <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="username"
-                backgroundColor={inputfieldColors}
-              ></Input>
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                Email <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="email"
-                type="email"
-                backgroundColor={inputfieldColors}
-              ></Input>
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                First Name <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="firstname"
-                backgroundColor={inputfieldColors}
-              ></Input>
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                Last Name <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="lastname"
-                backgroundColor={inputfieldColors}
-              ></Input>
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                Password <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="password"
-                type="password"
-                backgroundColor={inputfieldColors}
-              ></Input>
-              <Fieldset.HelperText color={secondaryText}>
-                Password must be at least 8 characters long, contain an uppercase
-                letter, a lowercase letter, a number, and a special character.
-                
-              </Fieldset.HelperText>
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label color={primaryText}>
-                Confirm Password{" "}
-                <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                variant="subtle"
-                onChange={handleChange}
-                name="confirmpassword"
-                type="password"
-                backgroundColor={inputfieldColors}
-              ></Input>
-            </Field.Root>
+      <Stack
+        gap="4"
+        alignSelf={"center"}
+        minW="100%"
+        style={{ paddingBottom: "2rem" }}
+        backgroundColor={secondary}
+        ref={containerRef}
+      >
+        <Fieldset.Root
+          animationName="fade-in"
+          animationDuration="slowest"
+          alignItems="center"
+          paddingTop="4rem"
+        >
+          <Stack gap="4" maxW="lg">
+            <Fieldset.Legend fontSize="4xl" color={primaryText}>
+              Account Details
+            </Fieldset.Legend>
+            <Fieldset.HelperText fontSize="xl" color={secondaryText}>
+              Please enter your details below to create an account on Dave.Save
+            </Fieldset.HelperText>
+            <Stack gap="2rem">
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  Username <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="username"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+              </Field.Root>
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  Email <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="email"
+                  type="email"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+              </Field.Root>
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  First Name <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="firstname"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+              </Field.Root>
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  Last Name <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="lastname"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+              </Field.Root>
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  Password <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="password"
+                  type="password"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+                <Fieldset.HelperText color={secondaryText}>
+                  Password must be at least 8 characters long, contain an
+                  uppercase letter, a lowercase letter, a number, and a special
+                  character.
+                </Fieldset.HelperText>
+              </Field.Root>
+              <Field.Root required>
+                <Field.Label color={primaryText}>
+                  Confirm Password{" "}
+                  <Field.RequiredIndicator></Field.RequiredIndicator>
+                </Field.Label>
+                <Input
+                  variant="subtle"
+                  onChange={handleChange}
+                  name="confirmpassword"
+                  type="password"
+                  backgroundColor={inputfieldColors}
+                ></Input>
+              </Field.Root>
+            </Stack>
+            <Button
+              onClick={onSubmit}
+              size="sm"
+              alignSelf="center"
+              width="100%"
+              color={buttonBackground}
+            >
+              <Text color={buttonText}>Sign Up.</Text>
+            </Button>
           </Stack>
-          <Button onClick={onSubmit} size="sm" alignSelf="center" width="100%" color={buttonBackground}>
-            <Text color={buttonText}>Sign Up.</Text>
-          </Button>
-        </Stack>
-      </Fieldset.Root>
-    </Stack>
+        </Fieldset.Root>
+      </Stack>
+    </>
   );
 };
 
