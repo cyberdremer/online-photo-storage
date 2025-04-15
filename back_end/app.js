a3d8b0870dbfecc7a7fd0d363cb191fa2295cd4a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-15 16:22:07 -0700

    Feat: nothing

--- back_end/app.js
+++ back_end/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const errorHandler = require("./middleware/error");
 const topLevelRoute = require("./routes/toplevel");
+const https = require("https");
 const cors = require("cors");
 require("dotenv").config();
 
-
-
-
 const app = express();
-app.use(express.urlencoded({limit: '10mb', extended: true }));
-app.use(cors({exposedHeaders: "Access-Control-Expose-Headers"}));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors({ exposedHeaders: "Access-Control-Expose-Headers" }));
 
 app.use(topLevelRoute);
 app.use(errorHandler);
+
+// https.createServer(app).listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
